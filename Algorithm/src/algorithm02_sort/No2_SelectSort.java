c0de8bf14d1e763331d45ecde5bfd0777070dad4
HumgTop <me@humg.top>
2020-08-23 11:32:20 +0800

    选择排序复习

--- Algorithm/src/algorithm02_sort/No2_SelectSort.java
+++ Algorithm/src/algorithm02_sort/No2_SelectSort.java
@@ -40,22 +40,20 @@ public class No2_SelectSort {
      * @return 已排序数组
      */
     public static int[] sort(int[] arr) {
-        //经过arr.length-1轮交换确定元素的正确位置（从数组首位到末位每轮确定一个位置的元素）
+        //经过arr.length-1轮交换确定元素的正确位置（从数组左到右每轮确定一个位置的元素）
         for (int i = 0; i < arr.length - 1; i++) {
             //每一轮进行比较大小的元素数是在递减的，从arr.length个递减到2个。
             //每一轮j的下限递增
-            //**每一轮循环都设置辅助变量：index存储最小值的下标
-            int index = i;   //先假定为最小值，记录下标
+            //** 每一轮循环都设置辅助变量：index存储最小值的下标
+            int minIndex = i;   //先假定为最小值，记录下标
             for (int j = i; j < arr.length; j++) {
-                if (arr[index] > arr[j]) {
-                    index = j;    //与后面所有元素进行比较，将index更新为较小元素的下标
+                if (arr[minIndex] > arr[j]) {
+                    minIndex = j;    //与后面所有元素进行比较，将index更新为较小元素的下标
                 }
             }
-            //**如果index的值发生更新，得到最小值的下标后，交换两者元素
-            if (index != i) {
-                int temp = arr[i];
-                arr[i] = arr[index];  //将最小的元素赋值
-                arr[index] = temp;
+            //** 如果index的值发生更新，得到最小值的下标后，交换两者元素
+            if (minIndex != i) {
+                swap(arr, minIndex, i);
             }
         }
         return arr;
@@ -64,31 +62,45 @@ public class No2_SelectSort {
     /**
      * 复习
      * 2020年8月18日15:08:15
+     * 2020年8月23日11:16:07
      *
      * @param arr
      * @return
      */
     public static int[] sortReview(int[] arr) {
-        //进行arr.length-1次比较，每次比较确定一个位置的元素（从前往后）
+        /*
+        经过arr.length-1轮排序，每轮确定一个位置的元素（从左往右）
+        每轮在后n-i个元素中找到最小值，放到下标为i+1的位置
+         */
         for (int i = 0; i < arr.length - 1; i++) {
-            int minIndex = i; //先假设一个最小值的下标
+            int minIndex = i;
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
             }
             if (minIndex != i) {
-                //确定arr[i]处的元素
-                int temp = arr[i];
-                arr[i] = arr[minIndex];
-                arr[minIndex] = temp;
+                swap(arr, i, minIndex);
             }
         }
         return arr;
     }
 
+    /**
+     * 对数组中2个位置的元素进行交换
+     *
+     * @param arr
+     * @param i
+     * @param j
+     */
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
     @Test
-    public void testReview(){
+    public void testReview() {
         int[] arr = {3, 9, -1, 10, 20}; //待排序数组
         int[] sort = sortReview(arr);
         System.out.println(Arrays.toString(sort));
