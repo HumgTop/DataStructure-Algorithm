f5c841945f55878646c7396f78b99bed4c806f87
HumgTop <me@humg.top>
2020-08-24 08:55:36 +0800

    插值查找 复习

--- Algorithm/src/algorithm03_search/No3_InterpolationSearch.java
+++ Algorithm/src/algorithm03_search/No3_InterpolationSearch.java
@@ -1,5 +1,7 @@
 package algorithm03_search;
 
+import org.junit.Test;
+
 /**
  * 插值查找（查找数组需是有序的）
  */
@@ -34,5 +36,41 @@ public class No3_InterpolationSearch {
             return mySearch(arr, value, mid + 1, right);
         } else
             return mid;
+    }
+
+    /**
+     * 复习
+     * 2020年8月24日08:45:05
+     *
+     * @param arr
+     * @param value
+     * @param low
+     * @param high
+     * @return
+     */
+    private static int searchReview(int[] arr, int value, int low, int high) {
+        //设置递归头
+        if (low > high) {
+            return -1;
+        }
+        //mid根据value的大小动态调整
+        int mid = low + (int) ((value - arr[low]) / (float) (arr[high] - arr[low]) * (high - low));
+
+        if (value < arr[mid]) {
+            return searchReview(arr, value, low, mid - 1);
+        } else if (value > arr[mid]) {
+            return searchReview(arr, value, mid + 1, high);
+        } else
+            return mid;
+    }
+
+    /**
+     * 测试复习方法
+     */
+    @Test
+    public void test1() {
+        int[] arr = {1, 8, 10, 89, 1000, 1234};
+        int index = searchReview(arr, 89, 0, arr.length - 1);
+        System.out.println(index);
     }
 }
