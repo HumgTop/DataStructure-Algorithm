e8aeb31fd0897bb3882c9b5ea0afe0e7c1591318
HumgTop <me@humg.top>
2020-08-31 10:09:37 +0800

    复习 | 插值查找

--- Algorithm/src/algorithm03_search/No3_InterpolationSearch.java
+++ Algorithm/src/algorithm03_search/No3_InterpolationSearch.java
@@ -23,11 +23,13 @@ public class No3_InterpolationSearch {
      */
     private static int mySearch(int[] arr, int value, int left, int right) {
         System.out.println("正在查找");
+        //递归头
         if (left > right || value < arr[0] || value > arr[arr.length - 1]) {
             return -1;
         }
 
-        int mid = left + (int) (((value - arr[left]) / (float) (arr[right] - arr[left])) * (right - left));
+        float coefficient = (value - arr[left]) / (float) (arr[right] - arr[left]);    //根据查找值value动态调整的系数
+        int mid = left + (int) (coefficient * (right - left));
         if (value < arr[mid]) {
             //左递归
             return mySearch(arr, value, left, mid - 1);
@@ -41,6 +43,7 @@ public class No3_InterpolationSearch {
     /**
      * 复习
      * 2020年8月24日08:45:05
+     * 2020年8月31日09:32:57
      *
      * @param arr
      * @param value
@@ -49,16 +52,18 @@ public class No3_InterpolationSearch {
      * @return
      */
     private static int searchReview(int[] arr, int value, int low, int high) {
-        //设置递归头
+        //递归头
         if (low > high) {
             return -1;
         }
-        //mid根据value的大小动态调整
-        int mid = low + (int) ((value - arr[low]) / (float) (arr[high] - arr[low]) * (high - low));
 
+        float coefficient = (value - arr[low]) / (float) (arr[high] - arr[low]);    //根据查找值value动态调整的系数
+        int mid = low + (int) (coefficient * (high - low));
         if (value < arr[mid]) {
+            //向左递归查找
             return searchReview(arr, value, low, mid - 1);
         } else if (value > arr[mid]) {
+            //向右递归查找
             return searchReview(arr, value, mid + 1, high);
         } else
             return mid;
@@ -70,7 +75,7 @@ public class No3_InterpolationSearch {
     @Test
     public void test1() {
         int[] arr = {1, 8, 10, 89, 1000, 1234};
-        int index = searchReview(arr, 89, 0, arr.length - 1);
+        int index = searchReview(arr, 2, 0, arr.length - 1);
         System.out.println(index);
     }
 }
