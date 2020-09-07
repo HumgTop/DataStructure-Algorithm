8fae7e30aba29740221a4185a49b7f487ca13fbf
HumgTop <me@humg.top>
2020-09-07 09:35:48 +0800

    常用算法 | 普里姆算法-2

--- Algorithm/src/common_Algorithm/No06_Prim_Algorithm.java
+++ Algorithm/src/common_Algorithm/No06_Prim_Algorithm.java
@@ -1,11 +1,13 @@
 package common_Algorithm;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 
 /**
  * 普里姆算法
+ * 求解最小生成树
  */
 public class No06_Prim_Algorithm {
     static MGraph graph;    //加权无向连通图
@@ -20,7 +22,7 @@ public class No06_Prim_Algorithm {
      * 使用普里姆算法得到最小生成树
      * 设 G=(V,E)是连通网， T=(U,D)是最小生成树， V,U 是顶点集合， E,D 是边的集合
      */
-    public static String prim() {
+    public static String myPrim() {
         int[] isVisited = new int[graph.vertexNum];
         StringBuilder D = new StringBuilder();
         ArrayList<Integer> U = new ArrayList<>();
@@ -39,17 +41,17 @@ public class No06_Prim_Algorithm {
                     if (isVisited[k] == 1) {
                         continue;
                     }
-                    int weight = graph.weight[U.get(j)][k];    //此处应拿到G-B之间的权值，实际拿的是B-B之间的权值
+                    int curWeight = graph.weight[U.get(j)][k];    //此处应拿到G-B之间的权值，实际拿的是B-B之间的权值
                     //保证至少赋值一次
-                    if (!flag && weight != 10000) {
-                        minWeight = weight;
+                    if (!flag && curWeight != 10000) {
+                        minWeight = curWeight;
                         uIndex = j;
                         vIndex = k;
                         flag = true;
                     }
-                    if (weight != 10000 && weight < minWeight) {
+                    if (curWeight != 10000 && curWeight < minWeight) {
                         //找到更优点，进行更新
-                        minWeight = weight;
+                        minWeight = curWeight;
                         uIndex = j;
                         vIndex = k;
                     }
@@ -61,6 +63,61 @@ public class No06_Prim_Algorithm {
         }
         System.out.println(U);
         return D.toString();
+    }
+
+    /**
+     * 网课demo
+     * 普里姆算法
+     * 设 G=(V,E)是连通网， T=(U,D)是最小生成树， V,U 是顶点集合， E,D 是边的集合
+     */
+    public void prim(int index) {
+        int[] isVisited = new int[graph.vertexNum]; //标记是否被访问过
+        isVisited[index] = 1;
+        //记录2个顶点的下标
+        int uIndex = -1;
+        int vIndex = -1;
+        int minWeight = 10000;    //先初始化minWeight
+
+        //共需添加 vertexNum-1 条边到D集合中
+        for (int k = 1; k < graph.vertexNum; k++) {
+            //遍历U集合（即V集合中已被访问过的节点）
+            for (int i = 0; i < graph.vertexNum; i++) {
+                //遍历V集合中未访问过的元素，找出边权值最小的ui和vj（此边添加到D集合，构成最小生成树）
+                for (int j = 0; j < graph.vertexNum; j++) {
+                    int curWeight = graph.weight[i][j]; //当前ui和vj间的权值
+                    if (isVisited[i] == 1 && isVisited[j] == 0 && curWeight < minWeight) {
+                        minWeight = curWeight;
+                        //记录下标是为了记录构成最小生成树的边
+                        uIndex = i;
+                        vIndex = j;
+                    }
+                }
+            }
+            System.out.printf("边<%s,%s>，权值：%d\n", graph.data[uIndex], graph.data[vIndex], minWeight);
+            isVisited[vIndex] = 1;  //将V集合中添加的顶点标记为1
+            minWeight = 10000;    //重置minWeight
+        }
+    }
+
+    @Test
+    public void primTest() {
+        //共有7个顶点
+        Character[] data = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        int vertexNum = 7;
+        //邻接矩阵（描述7个顶点间的连通关系）
+        //10000表示没有连通（权值很大，就不会选择这条路）
+        int[][] weight = new int[][]{
+                {10000, 5, 7, 10000, 10000, 10000, 2},
+                {5, 10000, 10000, 9, 10000, 10000, 3},
+                {7, 10000, 10000, 10000, 8, 10000, 10000},
+                {10000, 9, 10000, 10000, 10000, 4, 10000},
+                {10000, 10000, 8, 10000, 10000, 5, 4},
+                {10000, 10000, 10000, 4, 5, 10000, 6},
+                {2, 3, 10000, 10000, 4, 6, 10000},};
+
+        graph = new MGraph(vertexNum, data, weight);
+
+        prim(0);
     }
 
     public static void main(String[] args) {
@@ -80,13 +137,8 @@ public class No06_Prim_Algorithm {
 
         graph = new MGraph(vertexNum, data, weight);
 
-        System.out.println(prim());
+        System.out.println(myPrim());
     }
-}
-
-//创建最小生成树（图）
-class MinTree {
-
 }
 
 class MGraph {
