da7b91d98d89462f634f90d1c3391e4c0116b56b
HumgTop <me@humg.top>
2020-09-25 14:53:55 +0800

    LeetCode 队列

--- LeetcodeForPython/leetcode/editor/cn/[剑指 Offer 59 - I]滑动窗口的最大值.py
+++ LeetcodeForPython/leetcode/editor/cn/[剑指 Offer 59 - I]滑动窗口的最大值.py
@@ -1,5 +1,7 @@
 from typing import *
 from queue import Queue
+import collections
+import itertools
 
 
 # 给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
@@ -33,18 +35,42 @@ from queue import Queue
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        n = len(nums)
-        if n == 0: return []
-
+        deque = collections.deque()
         res = []
-        # 共有n-k个滑动窗口
-        for i in range(0, n - k + 1):
-            res.append(max(nums[i:i + k]))
+        n = len(nums)
+
+        # 滑动窗口的移动和队列的操作同时进行，滑动窗口操作n-k+1次，队列操作n次
+        for i, j in zip(range(1 - k, n - k + 1), range(n)):
+            # 如果左端出列元素刚好是当前窗口的最大值，则队首出列
+            if i > 0 and deque[0] == nums[i - 1]: deque.popleft()
+            # 添加当前j指针到队尾前，先将队列中小于j指针的元素移除
+            while deque and deque[-1] < nums[j]:
+                # 当前j指针入列前，从队尾移除队列中所有比它小的元素（保持队列单调非递减）
+                deque.pop()
+            deque.append(nums[j])
+            # 当i==0时，第一个滑动窗口最大值已经在deque队首，将其添加到res中
+            if i >= 0: res.append(deque[0])
+
         return res
+
+
+# class Solution:
+#     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+#         n = len(nums)
+#         if n == 0: return []
+#
+#         res = []
+#         # 共有n-k个滑动窗口
+#         for i in range(0, n - k + 1):
+#             res.append(max(nums[i:i + k]))
+#         return res
 
 
 # leetcode submit region end(Prohibit modification and deletion)
 
 
 if __name__ == '__main__':
+    nums = [1, 3, -1, -3, 5, 3, 6, 7]
+    k = 3
+    print(Solution().maxSlidingWindow(nums, k))
     pass
