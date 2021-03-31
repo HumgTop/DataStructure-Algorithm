eb05a8559fc8f5ddf47be84e10f3f1d9e4155b71
NUC <me@humg.top>
2021-03-31 23:30:53 +0800

    LeetCode

---
+++ LeetCode/src/leetcode/editor/cn/B_UniqueBinarySearchTrees.java
@@ -0,0 +1,19 @@
+package leetcode.editor.cn;
+
+
+public class B_UniqueBinarySearchTrees {
+    public static void main(String[] args) {
+
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int numTrees(int n) {
+            return 0;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
