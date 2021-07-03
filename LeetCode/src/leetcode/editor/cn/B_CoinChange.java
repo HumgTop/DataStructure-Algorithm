783a5c9e24279d9990d66237d83077d1204639d7
NUC <me@humg.top>
2021-07-03 11:23:47 +0800

    LeetCode

--- LeetCode/src/leetcode/editor/cn/B_CoinChange.java
+++ LeetCode/src/leetcode/editor/cn/B_CoinChange.java
@@ -6,26 +6,27 @@ import java.util.Arrays;
 
 public class B_CoinChange {
     public static void main(String[] args) {
-
-
+        Solution solution = new B_CoinChange().new Solution();
+        System.out.println(solution.coinChange(new int[]{2}, 3));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int coinChange(int[] coins, int amount) {
-            int[] dp = new int[amount + 1]; //dp[i]表示凑成金额i所需的最小硬币数
-            Arrays.fill(dp, Integer.MAX_VALUE - 100);
+            int[] dp = new int[amount + 1]; //dp[i]表示凑成金额i所需的最少硬币数
+            Arrays.fill(dp, Integer.MAX_VALUE);
             dp[0] = 0;
-            for (int i = 1; i <= amount; i++) {
+            for (int i = 1; i < dp.length; i++) {
                 for (int coin : coins) {
-                    if (i >= coin) {
-                        dp[i] = Math.min(dp[i], dp[i - coin] + 1);  //在本轮硬币选择中，选择最少可达成金额i的硬币数
+                    //如果不越界，且存在可转移的金额
+                    if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+                        //刷新最小硬币数
+                        dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
                     }
-                    //如果i<coins[j]，说明此硬币面值不能构成该金额
                 }
             }
 
-            return dp[amount] > amount ? -1 : dp[amount];
+            return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
