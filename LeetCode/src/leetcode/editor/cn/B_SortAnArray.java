1f6fc0ce7958dd8a720cb417d9781b49057f5fca
NUC <me@humg.top>
2021-07-07 22:22:19 +0800

    快速排序

--- LeetCode/src/leetcode/editor/cn/B_SortAnArray.java
+++ LeetCode/src/leetcode/editor/cn/B_SortAnArray.java
@@ -15,7 +15,37 @@ public class B_SortAnArray {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[] sortArray(int[] nums) {
-            return null;
+            //快速排序
+            quickSort(nums, 0, nums.length - 1);
+            return nums;
+        }
+
+        void quickSort(int[] nums, int left, int right) {
+            if (left >= right) return;
+            int partitionIdx = partition(nums, left, right);
+
+            quickSort(nums, left, partitionIdx);
+            quickSort(nums, partitionIdx + 1, right);
+        }
+
+        int partition(int[] nums, int left, int right) {
+            int pivotIdx = left;
+            int i = left + 1, j = i;
+            while (j <= right) {
+                if (nums[j] < nums[pivotIdx]) {
+                    swap(nums, i++, j);
+                }
+                j++;
+            }
+
+            swap(nums, pivotIdx, i - 1);
+            return i - 1;
+        }
+
+        void swap(int[] nums, int i, int j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
