79507d8d30452af12d7f59443b075ed0ec24a0ab
NUC <me@humg.top>
2021-07-23 10:54:04 +0800

    参考解法

--- LeetCode/src/leetcode/editor/cn/B_NumberOfIslands.java
+++ LeetCode/src/leetcode/editor/cn/B_NumberOfIslands.java
@@ -13,30 +13,31 @@ public class B_NumberOfIslands {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         int[][] moves = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int res = 0;
 
         public int numIslands(char[][] grid) {
-            //dfs
-            int res = 0;
             for (int i = 0; i < grid.length; i++) {
                 for (int j = 0; j < grid[0].length; j++) {
-                    if (dfs(grid, i, j)) res++;
+                    if (grid[i][j] == '1') {
+                        res++;
+                        dfs(grid, i, j);
+                    }
                 }
             }
 
             return res;
         }
 
-        //如果该点是新岛屿，返回true
-        boolean dfs(char[][] grid, int r, int c) {
-            if (r < 0 || r > grid.length - 1 || c < 0 || c > grid[0].length - 1 || grid[r][c] != '1') return false;
+        //由起点出发，将可到达的1区域染色为2
+        void dfs(char[][] grid, int r, int c) {
+            //越界返回
+            if (r < 0 || r > grid.length - 1 || c < 0 || c > grid[0].length - 1 || grid[r][c] != '1') return;
 
-            //标记此节点已访问
+            //染色
             grid[r][c] = '2';
             for (int[] move : moves) {
                 dfs(grid, r + move[0], c + move[1]);
             }
-
-            return true;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
